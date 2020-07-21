ecd515d9056493bcd405b5b641d3dcf442d00001
bansalraghav19 <18125@iiitu.ac.in>
2020-07-22 02:14:17 +0530

    gsbsb

--- src/components/ProfileComponent.js
+++ src/components/ProfileComponent.js
@@ -8,7 +8,7 @@ class Profile extends Component{
       render(){
             return(
                   <div className="container">
-                        <div className="col">
+                        <div className="col1">
                               <div className="card card1">
                                     <div className="dp">
                                           <img className="dp_img" src="./assets/dp.png"></img>
@@ -20,32 +20,50 @@ class Profile extends Component{
                                     </div>
                               </div>
                               <div className="card card2">
-                                    
-                                    <div className="price">
+                                    <div className="price-header">
                                           <div className="adjust">
-                                          <p className="title">Yearly</p>
-                                          <p className="sub-title">Subscription</p>
-                                    </div>
-                                          
-                                          <p className="cost">
+                                                <p className="title">Yearly</p>
+                                                <p className="sub-title">Subscription</p>
+                                          </div>
+                                          <div className="cost">
                                                 <span class="discount">$35</span>
                                                 $29.75
                                                 <span class="text">/mo</span>
-                                          </p>
+                                          </div>
                                     </div>
-                                    <div className="_content">
-                                          <p>Limited time offer for fight against COVID-19.</p>
-                                          Our most popular plan previously sold for $299 and is now only $10.6/month . This plan saves you over 60% in comparison to the monthly plan.
-                                          (prices are marked in USD)
-                                    </div>
-                                    <div className="btn">
-                                          <a className="btn2" href="#">Subscribe</a>
+                                    <div className="plan-details">
+                                          <div className="_content">
+                                                <span>Limited time offer for fight against COVID-19.</span>
+                                                <p>Our most popular plan previously sold for $299 and is now only $10.6/month . This plan saves you over 60% in comparison to the monthly plan.
+                                                (prices are marked in USD)</p>
+                                          </div>
+                                          <div className="btn">
+                                                <a className="btn2" href="#">Subscribe</a>
+                                          </div>
                                     </div>
                               </div>
                         </div>
-                        <div className="col">
-                              <div className="card card3">card3</div>
-                              <div className="card card4">card4</div>
+                        <div className="col2">
+                              <div className="card card1">
+                                    <div className="heading">
+                                          <h2>Account Info</h2>
+                                    </div>
+                                    <div className="about-info">
+                                          <ul>
+                                                <li>
+                                                      <label>Email</label>
+                                                      <p>raghavrocking1@gmail.com</p>
+                                                </li>
+                                                <li>  
+                                                      <label>Phone Number</label>
+                                                      <p>+91 9478121646</p>
+                                                </li>
+                                          </ul>
+                                    </div>
+                              </div>
+                              <div className="card card2">
+                                    B
+                              </div>
                         </div>
                   </div>
             );
